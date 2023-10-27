aa3fdf593770476f8e33495ea186eec632713e77
Harish Kumar Gangula <harish@sanketika.in>
2023-10-27 09:08:50 +0530

    Issue #000 feat: Added api monitoring template

--- data-generation/scripts/index.js
+++ data-generation/scripts/index.js
@@ -3,6 +3,8 @@ const fs = require('fs-extra');
 const jsf = require('json-schema-faker');
 const _ = require('lodash');
 const { faker } = require('@faker-js/faker');
+const prompts = require('prompts');
+const path = require('path');
 
 const generate = (schema) => {
     return jsf.generate(schema);
@@ -102,18 +104,18 @@ const applyBooleanConditions = (conditions, event) => {
 
 const applyConditions = (conditions, event) => {
     for (const field in conditions) {
-        const fieldDate = _.get(event, field)
+        const fieldData = _.get(event, field)
         const fieldConditions = _.get(conditions, field)
-        if (_.isNumber(fieldDate)) {
+        if (_.isNumber(fieldData)) {
             const data = applyNumberConditions(fieldConditions, event)
             _.set(event, field, data)
-        } else if(_.isString(fieldDate)) {
+        } else if(_.isString(fieldData)) {
             const data = applyStringConditions(fieldConditions, event)
             _.set(event, field, data)
-        } else if(_.isArray(fieldDate)) {
+        } else if(_.isArray(fieldData)) {
             const data = applyArrayConditions(fieldConditions, event)
             _.set(event, field, data)
-        } else if(_.isBoolean(fieldDate)) {
+        } else if(_.isBoolean(fieldData)) {
             const data = applyBooleanConditions(fieldConditions, event)
             _.set(event, field, data)
         }
@@ -124,23 +126,43 @@ const applyConditions = (conditions, event) => {
 }
 
 
-const applyDenorm = () => {
-
-// apply denorm
+const applyDenorm = (configs, event) => {
+    let data = event
+    for(const config of configs) {
+        if(_.isObject(config.values[0])) {
+            let pathObject = _.get(event, config?.path)
+            if(!_.isEmpty(config.values[0]?.weight)) {
+                let values = _.map(config.values, (d) => {
+                    return ({...d, ...{ weight: d?.weight/ 10 }})
+                 })
+                pathObject = {...pathObject, ...faker.helpers.weightedArrayElement(values)}
+            } else {
+                pathObject = {...pathObject, ..._.sample(config.values)}
+            }
+            data = _.set(data, config.path, pathObject)
+        } else {
+            data = _.set(data, config.path, _.sample(config.values))   
+        }
+    }
+    return  data;
 
 }
 
 
-const start = async (templatePath, count = 100, outputPath) => {
+const start = async (templatePath, count = 1000, outputPath) => {
     try {
-        const writeStream = fs.createWriteStream("../output/vsk/student-attendance.json")
+        const writeStream = fs.createWriteStream(outputPath)
         let template = await fs.readJson(templatePath);
         const schema = template?.template?.schema;
         const conditions = template?.template?.conditions;
+        const denormConfig = template?.template?.denormConfig
         for (let index = 0; index < count; index++) {
             let event = generate(schema)
             if (!_.isEmpty(conditions)) {
                 event = applyConditions(conditions, event)
+            }
+            if(!_.isEmpty(denormConfig)) {
+                event = applyDenorm(denormConfig, event)
             }
             writeStream.write(JSON.stringify(event)+'\n')
         }
@@ -150,5 +172,37 @@ const start = async (templatePath, count = 100, outputPath) => {
     }
 }
 
-
-start("/Users/harishkumargangula/Documents/GitHub/obsrv-adhoc-scripts/data-generation/templates/vsk/student-attendance.json")
+const questions = [
+    {
+        type: 'select',
+        name: 'template',
+        message: 'Pick a template to generate events',
+        choices: [
+          { title: 'VSK', value: 'vsk' },
+          { title: 'api monitoring', value: 'api-monitoring' }
+        ],
+        initial: 1
+      },
+      {
+        type: 'number',
+        name: 'count',
+        message: 'How many events',
+        initial: 1000,
+        style: 'default',
+        min: 1
+      }
+  ];
+  
+  (async () => {
+    const response = await prompts(questions);
+  
+    const templatesDir = path.join(__dirname, '..' , 'templates', response.template)
+    const files = await fs.readdir(templatesDir)
+    for(const file of files) {
+        const templatePath = path.join(templatesDir, file);
+        const outputEventsPath = path.join(__dirname, '..' , 'output', response.template, file)
+        await fs.ensureFile(outputEventsPath)
+        start(templatePath, response?.count, outputEventsPath)
+    }
+  })();
+  
