788ca2329d3dbe37fa48b83c0820954de1300962
Aniket Sakinala <aniket@sanketika.in>
2024-03-07 11:21:10 +0530

    Sanketika-Obsrv/issue-tracker#20: fix: add metrics and fix body schema

--- saas-data-migration/migrate_data.py
+++ saas-data-migration/migrate_data.py
@@ -12,7 +12,7 @@ from azure.storage.blob import BlobClient, ContainerClient
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(format=FORMAT, filename='onest_data_transfer.log', encoding='utf-8', level=logging.INFO)
 
-api_url = "http://localhost:3000/data/v2/in/onest-v3-api-telemetry" # need to update with correct topic name
+api_url = "http://localhost:3000/network-observability/v1/in/onest-network-telemetry-v3" # need to update with correct topic name
 headers = {
   'Content-Type': 'application/json'
 }
@@ -29,19 +29,30 @@ if older_data.exists():
 connection_string = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix=core.windows.net".format(azure_storage_account_name, azure_storage_account_key)
 container_name = "obsrv-onest"
 container = ContainerClient.from_connection_string(conn_str=connection_string, container_name=container_name)
-blob_list = container.list_blob_names(name_starts_with="telemetry-data/ingest/")
+blob_list = container.list_blob_names(name_starts_with="telemetry-data/ingest/onest-network-telemetry/")
+event_counter = 0
+file_counter = 0
 for blob_ in blob_list:
     if blob_ not in older_files:
         blob = BlobClient.from_connection_string(conn_str=connection_string, container_name=container_name, blob_name=blob_)
         blob_data = blob.download_blob()
         data = gzip.decompress(blob_data.readall())
-        json_data = json.loads(data)
-        json_data = {"data": json_data}
-        response = requests.request("POST", api_url, headers=headers, json=json_data)
-        logging.info("File %s, API response code: %s, API response msg: %s", blob_, response.status_code, response.text)
-        with open("logger.log", "a+") as f:
-            f.write("{},{},{}".format(blob_, response.status_code, response.text))
-        print(blob_, response.status_code, response.text)
+        event_counter_inner = 0
+        batch_counter = 0
+        for batch in data.splitlines():
+            json_data = {"data": json.loads(batch)}
+            batch_len = len(json_data["data"]["events"])
+            event_counter_inner += batch_len    
+            response = requests.request("POST", api_url, headers=headers, json=json_data)
+            batch_counter += 1
+            logging.info("File %s, Batch id %s, batch size: %s, API response code: %s, API response msg: %s", blob_, response.status_code, response.text)
+            with open("logger.log", "a+") as f:
+                f.write("{},{},{},{}".format(blob_, json_data["data"]["id"], batch_len, response.status_code))
+            print(blob_, json_data["data"]["id"], batch_len, response.status_code)
+        file_counter += 1
+        event_counter += event_counter_inner
     else:
         print(blob_, "already processed, skipping")
-
+print("total files processed: ", file_counter)
+print("total batches processed: ", batch_counter)
+print("total events processed: ", event_counter)
