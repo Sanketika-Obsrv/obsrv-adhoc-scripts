894db3fb896bb9e50ddfb52f01b32a154a8d5d3a
Aniket Sakinala <aniket@sanketika.in>
2024-03-08 11:45:45 +0530

    Sanketika-Obsrv/issue-tracker#20: fix: add multithreading and config

--- saas-data-migration/migrate_data.py
+++ saas-data-migration/migrate_data.py
@@ -1,58 +1,48 @@
-import os
 import gzip
 import json
-import time
+import yaml
 import logging
-from pathlib import Path
+import datetime
+from multiprocessing import Pool
 
 import requests
-import pandas as pd
 from azure.storage.blob import BlobClient, ContainerClient
 
+start_ = datetime.datetime.now()
 FORMAT = '%(asctime)s %(message)s'
 logging.basicConfig(format=FORMAT, filename='onest_data_transfer.log', encoding='utf-8', level=logging.INFO)
 
-api_url = "http://localhost:3000/network-observability/v1/in/onest-network-telemetry-v3" # need to update with correct topic name
+with open("config.yaml", "r") as f:
+    config = yaml.load(f, yaml.FullLoader)
+
+api_url = config["endpoint"] + config["dataset"]
 headers = {
   'Content-Type': 'application/json'
 }
 
-azure_storage_account_name=os.getenv("AZURE_ACCOUNT_NAME")
-azure_storage_account_key=os.getenv("AZURE_ACCOUNT_KEY")
+def process_blob(blob_):
+    blob = BlobClient.from_connection_string(conn_str=connection_string, container_name=config["container_name"], blob_name=blob_)
+    blob_data = blob.download_blob()
+    data = gzip.decompress(blob_data.readall())
+    for batch in data.splitlines():
+        json_data = {"data": json.loads(batch)}
+        batch_len = len(json_data["data"]["events"])
+        if batch_len == 0:
+            continue
+        response = requests.post(api_url, headers=headers, json=json_data)
+        logging.info("File %s, Batch id %s, batch size: %s, API response code: %s, API response msg: %s", blob_, json_data["data"]["id"], batch_len, response.status_code, response.text)
+        with open("logger.log", "a+") as f:
+            f.writelines("{},{},{},{}\n".format(blob_, json_data["data"]["id"], batch_len, response.status_code))
+        print(blob_, json_data["data"]["id"], batch_len, response.status_code)
 
-older_files = []
-older_data = Path("logger.log")
-if older_data.exists():
-    df = pd.read_csv(older_data, names=["file", "status_code", "response_text"])
-    df = df[df["status_code"]==200]
-    older_files = df["file"].unique().tolist()
-connection_string = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix=core.windows.net".format(azure_storage_account_name, azure_storage_account_key)
-container_name = "obsrv-onest"
-container = ContainerClient.from_connection_string(conn_str=connection_string, container_name=container_name)
-blob_list = container.list_blob_names(name_starts_with="telemetry-data/ingest/onest-network-telemetry/")
-event_counter = 0
-file_counter = 0
-for blob_ in blob_list:
-    if blob_ not in older_files:
-        blob = BlobClient.from_connection_string(conn_str=connection_string, container_name=container_name, blob_name=blob_)
-        blob_data = blob.download_blob()
-        data = gzip.decompress(blob_data.readall())
-        event_counter_inner = 0
-        batch_counter = 0
-        for batch in data.splitlines():
-            json_data = {"data": json.loads(batch)}
-            batch_len = len(json_data["data"]["events"])
-            event_counter_inner += batch_len    
-            response = requests.request("POST", api_url, headers=headers, json=json_data)
-            batch_counter += 1
-            logging.info("File %s, Batch id %s, batch size: %s, API response code: %s, API response msg: %s", blob_, json_data["data"]["id"], batch_len, response.status_code, response.text)
-            with open("logger.log", "a+") as f:
-                f.writeline("{},{},{},{}".format(blob_, json_data["data"]["id"], batch_len, response.status_code))
-            print(blob_, json_data["data"]["id"], batch_len, response.status_code)
-        file_counter += 1
-        event_counter += event_counter_inner
-    else:
-        print(blob_, "already processed, skipping")
-print("total files processed: ", file_counter)
-print("total batches processed: ", batch_counter)
-print("total events processed: ", event_counter)
+connection_string = "DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix=core.windows.net".format(config["azure_storage_account_name"], config["azure_storage_account_key"])
+container = ContainerClient.from_connection_string(conn_str=connection_string, container_name=config["container_name"])
+start_date = config["start_date"]
+end_date = config["end_date"]
+while start_date < end_date:
+    blob_list = container.list_blob_names(name_starts_with=config["prefix"] + start_date.strftime("%Y-%m-%d"))
+    with Pool(config["cpus"]) as p:
+        p.map(process_blob, [blob_ for blob_ in blob_list])
+    start_date += datetime.timedelta(1)
+end_ = datetime.datetime.now()
+print("time taken: ", end_-start_)
