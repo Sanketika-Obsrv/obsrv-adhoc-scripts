7517e74c8e6ef53b604c29f6970dbf6625ac129a
SurabhiAngadi <surabhia@sanketika.in>
2024-03-28 11:06:22 +0530

    Sanketika-Obsrv/issue-tracker#51: wrapper-api modifications

--- flink/flink-Dockerfile/wrapper-api.py
+++ flink/flink-Dockerfile/wrapper-api.py
@@ -3,24 +3,21 @@ import zipfile
 import os
 import shutil
 from io import BytesIO
-from flask import Flask, jsonify, request, make_response
 from urllib.parse import urlparse
 
-app = Flask(__name__)
-
-@app.route('/extract', methods=['POST'])
 def unzip():
     try:
-        request_data = request.get_json()
-        url = request_data.get('s3_url')
+        # url = "https://custom-jobs.s3.us-east-2.amazonaws.com/application.zip"
+        url = os.environ.get("FILEPATH")
         parsed_url = urlparse(url)
         bucket=parsed_url.netloc.split('.')[0]
         file=parsed_url.path.lstrip('/')
         print("Bucket: ",bucket," File: ",file)
+        # session = boto3.Session(profile_name='your_profile_name')
         resource = boto3.resource('s3', region_name='us-east-2')
         obj = resource.Object(bucket_name=bucket, key=file)
         buffer = BytesIO(obj.get()["Body"].read()) 
-        tmp = 'tmp/'
+        tmp = '/temp' # ADD UUID 
         if not os.path.exists(tmp):
             os.makedirs(tmp, exist_ok=True)
         with zipfile.ZipFile(buffer, "r") as zip_ref:
@@ -28,21 +25,21 @@ def unzip():
         if len(os.listdir(tmp))==0:
             print("Failed to extract!")
         else:
-            print("Extracted.")
+            print(f"Extracted.")
         for root, dirs, files in os.walk(tmp):
             for file in files:
                 if file.endswith(".jar"):
                     source_path = os.path.join(root, file)
-                    destination_dir = 'lib/'
+                    destination_dir = '/opt/flink/lib'
+                    print(os.path.abspath(destination_dir))
                     if not os.path.exists(destination_dir):
                         os.makedirs(destination_dir, exist_ok=True)
                     shutil.copy(source_path, destination_dir)
                     print(f"Copied {file} to {destination_dir}")
-        return make_response("Copied jars", 200)
     except Exception as e:
         print(e)
-        return make_response("Error occurred: ",str(e), 500)
+        print("Error occurred: ",str(e))
     
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    unzip()
     
