d1966b97e231f88a721d6113a0ff37fa1e24dc89
Ravi Mula <ravismula@users.noreply.github.com>
2024-12-22 18:22:40 +0530

    update dataformats

--- sample-datasets/New York Taxi Data/app.js
+++ sample-datasets/New York Taxi Data/app.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const csvSplitStream = require('csv-split-stream');
 const _ = require('lodash');
 const dayjs = require('dayjs');
-const {v4: uuidv4} = require('uuid')
+const { v4: uuidv4 } = require('uuid')
 const { faker } = require('@faker-js/faker');
 
 
@@ -74,37 +74,37 @@ function getRandomDate(time) {
 
 
 for (let index = 1; index <= 5000; index++) {
-  const data = fs.readFileSync(`chunk-${index}.json`, 'utf-8')
+  const data = fs.readFileSync(`events/chunk-${index}.json`, 'utf-8')
   let parsed = JSON.parse(data);
   parsed = _.map(parsed, item => {
-    let newItem =  {
+    let newItem = {
       "tripID": uuidv4(),
-      "VendorID": item.VendorID,
+      "VendorID": item.VendorID.toString(),
       "tpep_pickup_datetime": item.tpep_pickup_datetime,
       "tpep_dropoff_datetime": item.tpep_dropoff_datetime,
-      "passenger_count": item.passenger_count,
-      "trip_distance": item.trip_distance,
-      "RatecodeID": item.RatecodeID,
+      "passenger_count": item.passenger_count.toString(),
+      "trip_distance": item.trip_distance.toString(),
+      "RatecodeID": item.RatecodeID.toString(),
       "store_and_fwd_flag": item.store_and_fwd_flag,
-      "PULocationID": item.PULocationID,
-      "DOLocationID": item.DOLocationID,
-      "payment_type": item.payment_type,
+      "PULocationID": item.PULocationID.toString(),
+      "DOLocationID": item.DOLocationID.toString(),
+      "payment_type": item.payment_type.toString(),
       "primary_passenger": {
         "email": faker.internet.email(),
         "mobile": faker.phone.number(),
       },
       "fare_details": {
-        "fare_amount": item.fare_details.fare_amount,
-        "extra": item.fare_details.extra,
-        "mta_tax": item.fare_details.mta_tax,
-        "tip_amount": item.fare_details.tip_amount,
-        "tolls_amount": item.fare_details.tolls_amount,
-        "improvement_surcharge": item.fare_details.improvement_surcharge,
-        "total_amount": item.fare_details.total_amount,
-        "congestion_surcharge": item.fare_details.congestion_surcharge
+        "fare_amount": parseInt(item.fare_details.fare_amount),
+        "extra": parseFloat(item.fare_details.extra),
+        "mta_tax": parseFloat(item.fare_details.mta_tax),
+        "tip_amount": parseInt(item.fare_details.tip_amount),
+        "tolls_amount": parseInt(item.fare_details.tolls_amount),
+        "improvement_surcharge": parseFloat(item.fare_details.improvement_surcharge),
+        "total_amount": parseInt(item.fare_details.total_amount),
+        "congestion_surcharge": parseInt(item.fare_details.tip_amount / 5)
       }
     }
     return newItem;
   })
-  fs.writeFileSync(`chunk-${index}.json`, JSON.stringify(parsed))
+  fs.writeFileSync(`events/chunk-${index}.json`, JSON.stringify(parsed))
 }
--- sample-datasets/New York Taxi Data/ingest.js
+++ sample-datasets/New York Taxi Data/ingest.js
@@ -4,48 +4,49 @@ const fs = require('fs')
 const axiosRetry = require('axios-retry').default;;
 
 axiosRetry(axios, {
-    retries: 10, // number of retries
-    retryDelay: (retryCount) => {
-        console.log(`retry attempt: ${retryCount}`);
-        return retryCount * 2000; // time interval between retries
-    }
+  retries: 10, // number of retries
+  retryDelay: (retryCount) => {
+    console.log(`retry attempt: ${retryCount}`);
+    return retryCount * 2000; // time interval between retries
+  }
 });
 
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
+function randomIntFromInterval(min, max) { // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 const delay = () => {
-    const delayIn = randomIntFromInterval(6000, 20000)
-    console.log(`After delay of ${delayIn} ms`)
-    return new Promise(resolve => setTimeout(() => { resolve()}, delayIn))
+  const delayIn = randomIntFromInterval(6000, 20000)
+  console.log(`After delay of ${delayIn} ms`)
+  return new Promise(resolve => setTimeout(() => { resolve() }, delayIn))
 }
 
 const ingest = async () => {
-    for (let index =1; index <= 3000; index++) {
-        const data = {
-            "data": {
-              "id": uuidv4(),
-              "events":[]}
-        }
-        const events = fs.readFileSync(`chunk-${index}.json`, 'utf-8')
-        data.data.events = JSON.parse(events);
-        console.log(`start chunk ${index}`)
-        let config = {
-            method: 'post',
-            maxBodyLength: Infinity,
-            url: 'http://localhost:6000/data/v1/in/new-york-taxi-data',
-            headers: { 
-              'accept': 'application/json, text/plain, */*', 
-              'content-type': 'application/json',
-            },
-            data : JSON.stringify(data)
-          };
-          await axios.request(config)
-          console.log(`completed chunk ${index}`)
-          await delay()
-        
+  for (let index = 2; index <= 5; index++) {
+    const data = {
+      "data": {
+        "id": uuidv4(),
+        "events": []
+      }
     }
+    const events = fs.readFileSync(`chunk-${index}.json`, 'utf-8')
+    data.data.events = JSON.parse(events);
+    console.log(`start chunk ${index}`)
+    let config = {
+      method: 'post',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:6000/data/v1/in/new-york-taxi-data',
+      headers: {
+        'accept': 'application/json, text/plain, */*',
+        'content-type': 'application/json',
+      },
+      data: JSON.stringify(data)
+    };
+    await axios.request(config)
+    console.log(`completed chunk ${index}`)
+    await delay()
+
+  }
 }
 
 ingest().then().catch(er => console.log(er))
